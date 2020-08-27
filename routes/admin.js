5c0830cc80c995cc089d7a1e2161395ef308860c
Sahil Phondekar <sahil.jphondekar@gmail.com>
2020-08-27 21:20:28 +0530

    08-serving-the-html-files

--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 // Create an express router
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send(
-        '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-    );
+    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
 });
 
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
