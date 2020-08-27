760aafc8abf6bf432e94ffa23090fc1c603ec108
Sahil Phondekar <sahil.jphondekar@gmail.com>
2020-08-27 21:05:56 +0530

    06-filtering-paths

--- app.js
+++ app.js
@@ -11,8 +11,12 @@ const shopRoutes = require('./routes/shop');
 // use the bodyparser middleware
 app.use(bodyParser.urlencoded({extended: false})); // returns middleware that only parses urlencoded bodies
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not Found</h1>');
+});
 
 // create an express server and listen to port 3000
 app.listen(3000);
