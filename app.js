784ae10960dd06c6c3c57c1c483adb58f962685f
Sahil Phondekar <sahil.jphondekar@gmail.com>
2020-08-30 11:12:37 +0530

    05-working-on-layout-with-partials

--- app.js
+++ app.js
@@ -3,21 +3,11 @@ const path = require("path");
 // import statements: NODE npm modules
 const express = require("express");
 const bodyParser = require("body-parser");
-const expressHbs = require('express-handlebars');
 
 // create an exress app
 const app = express();
 
-app.engine(
-  "hbs",
-  expressHbs({
-    layoutsDir: "views/layouts/",
-    defaultLayout: "main-layout",
-    extname: "hbs",
-  })
-);
-
-app.set("view engine", "hbs");
+app.set("view engine", "ejs");
 app.set("views", "views");
 
 const adminData = require("./routes/admin");
