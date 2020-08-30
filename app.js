09904b77185186f1888261d42df2c93f948f893a
Sahil Phondekar <sahil.jphondekar@gmail.com>
2020-08-30 11:48:00 +0530

    01-finished-the-controller

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const errorController = require('./controllers/error');
+
 // create an exress app
 const app = express();
 
@@ -21,9 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errorController.get404);
 
 // create an express server and listen to port 3000
 app.listen(3000);
